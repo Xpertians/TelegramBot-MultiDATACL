60da4ec15e2dadf3cb18ee5b9289ee1da536e3ac
EC2 Default User <ec2-user@ip-172-31-44-40.ec2.internal>
2018-12-12 22:53:44 +0000

    Adding cert

--- hook.php
+++ hook.php
@@ -4,19 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '785904207:AAFDR2V8Y-M25Aqx7bRjvxrj0ROPGA65u8I';
 $bot_username = 'multidatacl_bot';
-$hook_url     = 'https://telegram.opendatacollector.com/hook.php';
 
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    // Set webhook
-    //$result = $telegram->setWebhook($hook_url);
-    $result = $telegram->setWebhook($hook_url, ['certificate' => 'cert.pem']);
-    if ($result->isOk()) {
-        echo $result->getDescription();
-    }
+    // Handle telegram webhook request
+    $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
     // log telegram errors
     // echo $e->getMessage();
 }

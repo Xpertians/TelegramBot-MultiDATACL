40d5fa44e6652d21d2dce34f3b712a7b35866a9e
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-12-12 23:12:48 +0000

    Adding my self as admin

--- hook.php
+++ hook.php
@@ -1,15 +1,12 @@
 <?php
-// Load composer
 require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '785904207:AAFDR2V8Y-M25Aqx7bRjvxrj0ROPGA65u8I';
 $bot_username = 'multidatacl_bot';
 
 try {
-    // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-
-    // Handle telegram webhook request
+    $telegram->enableAdmin(61259010);
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!

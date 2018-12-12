fc1b6529c481b21febd01ace806c97ddda4720b1
EC2 Default User <ec2-user@ip-172-31-44-40.ec2.internal>
2018-12-12 22:47:55 +0000

    Adding vendor and set

---
+++ set.php
@@ -0,0 +1,21 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '785904207:AAFDR2V8Y-M25Aqx7bRjvxrj0ROPGA65u8I';
+$bot_username = 'multidatacl_bot';
+$hook_url     = 'https://telegram.opendatacollector.com/hook.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}

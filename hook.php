547dd6e487930bf21edca6feecb584c4680ac390
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-12-12 23:30:35 +0000

    Adding more commands

--- hook.php
+++ hook.php
@@ -4,9 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 $bot_api_key  = '785904207:AAFDR2V8Y-M25Aqx7bRjvxrj0ROPGA65u8I';
 $bot_username = 'multidatacl_bot';
 
+$commands_paths = [
+    __DIR__ . '/Commands',
+];
+
 try {
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
     $telegram->enableAdmin(61259010);
+    $telegram->addCommandsPaths($commands_paths);
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
